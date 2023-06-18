e2af6b26ce2357d5938c3c37bafd7c12c162d026
Marin <marin.muktic3@gmail.com>
2023-06-18 16:35:19 +0200

    component rendering fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,10 @@
 import "./App.css";
 import Video from "./Components/Video";
-import Fetch from "./Components/Fetch";
 
 function App() {
   return (
     <div className="App">
       <Video />
-      <Fetch />
     </div>
   );
 }
