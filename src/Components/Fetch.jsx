2465449447d00a828e733f670572cdefa120174f
Marin <marin.muktic3@gmail.com>
2023-06-18 12:21:10 +0200

    setting fetch data

--- src/Components/Fetch.jsx
+++ src/Components/Fetch.jsx
@@ -1,9 +1,24 @@
-import React from 'react'
+import React, { useEffect } from "react";
 
 const Fetch = () => {
-  return (
-    <div>Fetch</div>
-  )
-}
+  useEffect(() => {
+    getWeatherData();
+  }, []);
 
-export default Fetch
+  const getWeatherData = async () => {
+    const response = await fetch(
+      `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${process.env.REACT_APP_API_KEY}&units=metric`
+    );
+    const data = await response.json();
+    console.log(data);
+  };
+  return (
+    <div>
+      <h2>Weather fetch</h2>
+      <input type="text" value={location} />
+      <button onClick={getWeatherData}>SEARCH</button>
+    </div>
+  );
+};
+
+export default Fetch;
