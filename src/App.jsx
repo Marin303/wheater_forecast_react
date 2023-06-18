b088af0e68d7bc3d7ee25b707bc0e7a9c0b400f8
Marin <marin.muktic3@gmail.com>
2023-06-18 11:50:08 +0200

    intial commit with background video

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import Main from "./Components/Main";
+import Fetch from "./Components/Fetch"
+
+function App() {
+  return (
+    <div className="App">
+      <Main />
+      <Fetch/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/Main.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import videoBackground from "../Images/Raindrops_4K_Living_Background.mp4";
+
+const Main = () => {
+  return (
+    <div>
+      <video autoPlay muted loop className="backgroundVideo">
+        <source src={videoBackground} type="video/mp4" />
+      </video>
+      <h1>Wheater Forecast</h1>
+    </div>
+  );
+};
+
+export default Main;
